a2795706921af40ddf6af1eebc4937c0ede40ca8
RkdJIHYE <kangbs210210@gmail.com>
2026-01-29 14:41:26 +0900

    Fix:명언게시판

--- Wiseboard/src/main/java/Main.java
+++ Wiseboard/src/main/java/Main.java
@@ -6,11 +6,23 @@ public class Main {
         //스캐너를 이용하여, 키보드에 입력을 받는다.
         Scanner sc = new Scanner(System.in);
 
-
+        //1단계
         System.out.println("==명언 탭==");
         System.out.print("명령) ");
         String cmd = sc.nextLine();
 
         System.out.println(cmd);
+
+        //2단계 (등록)
+
+        System.out.print("명언 : ");
+        String wiseSaying = sc.nextLine();
+        System.out.print("작가 : ");
+        String Author = sc.nextLine();
+
+        System.out.print("명령) ");
+        cmd = sc.nextLine();
+
+
     }
 }
