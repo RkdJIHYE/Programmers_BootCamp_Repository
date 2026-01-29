fe550200b28b6796c9f4ac5ecd219140a8a608fe
RkdJIHYE <kangbs210210@gmail.com>
2026-01-29 14:03:52 +0900

    Fix: exam2 수정

--- exam2/src/main/java/org/example/Main.java
+++ exam2/src/main/java/org/example/Main.java
@@ -1,33 +1,24 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        int[] arr = new int[3];
+        arr[0] = 10;
+        arr[1] = 20;
+        arr[2] = 30;
+        System.out.println(arr[0] + arr[1] + arr[2]);
+        System.out.println(arr.length); // 고정길이
 
-        Person p1 = new Person();
-
-        p1.age = 20;
-        p1.name = "홍길동";
-        p1.home = "서울";
-        p1.height = 180.5;
-        p1.introduce();
-        p1.addage();
-        p1.introduce();
-
-    }
-
-}
-
-class Person {
-    int age;
-    String name;
-    String home;
-    double height;
-
-    public void introduce() {
-        System.out.println("나이: " + age + " 이름: " + name + " 사는 곳: " + home + " 키: " + height);
-    }
-
-    public void addage(){
-        age++;
+        List<Integer> al = new ArrayList<>();
+        al.add(10); // 0
+        al.add(20); // 1
+        al.add(30); // 2
+        System.out.println(al.get(0) + al.get(1) + al.get(2));
+        System.out.println(al.size()); // 길이 : 3
+        al.add(40); // 3
+        System.out.println(al.size()); // 길이 : 4
     }
 }
