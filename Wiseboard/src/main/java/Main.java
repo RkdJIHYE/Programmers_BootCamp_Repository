56e17e2dc3a94789cee669eded28a3887fcb0164
RkdJIHYE <kangbs210210@gmail.com>
2026-01-29 15:42:31 +0900

    Feat: 5단계 추가

--- Wiseboard/src/main/java/Main.java
+++ Wiseboard/src/main/java/Main.java
@@ -5,10 +5,11 @@ public class Main {
 
         //스캐너를 이용하여, 키보드에 입력을 받는다.
         Scanner sc = new Scanner(System.in);
-
         //1단계
         System.out.println("==명언 탭==");
-        int cnt =1;
+        int cnt =0;
+        String Author="";
+        String wiseSaying = "";
         while (true){
             System.out.print("명령) ");
             String cmd = sc.nextLine();
@@ -17,17 +18,17 @@ public class Main {
             }
             //2단계 (등록)
             if (cmd.equals("등록")) {
+                ++cnt;
                 System.out.print("명언 : ");
-                String wiseSaying = sc.nextLine();
+                wiseSaying = sc.nextLine();
                 System.out.print("작가 : ");
-                String Author = sc.nextLine();
+                Author = sc.nextLine();
                 System.out.println(cnt+"번이 등록되었습니다.");
-                cnt++;
-
             }
-            else{
-                System.out.print("명령) ");
-                cmd = sc.nextLine();
+            else if (cmd.equals("목록")){
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+                System.out.println(cnt+" / "+Author+" / "+wiseSaying);
             }
 
         }
