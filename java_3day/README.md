12d25f00f29f9c6cf539b98ea593eccb8763b15d
RkdJIHYE <kangbs210210@gmail.com>
2026-01-30 17:09:46 +0900

    Feat:D-day 3 readme 정리

---
+++ java_3day/README.md
@@ -0,0 +1,6 @@
+# 2026.1.30 Fri , TIL 정리 
+오늘 배운 내용 정리해 notion에 올려두었다.
+# https://leeward-appeal-325.notion.site/2026-1-30-D-4-Day03-Java-2f85aa1db8b78055a77cd6ab158d3fc9?pvs=73
+
+
+
--- java_3day/src/main/java/org/example/Main.java
+++ java_3day/src/main/java/org/example/Main.java
@@ -3,49 +3,43 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        무기 a무기 = new 칼();
-        a무기.공격();
-        // 출력 : 칼로 공격합니다.
-
-        a무기 = new 활();
-        a무기.공격();
-
-        a무기 = new 도끼();
-        a무기.공격();
-        // 출력 : 활로 공격합니다.
-
-        a무기 = new 몽둥이();
-        a무기.공격();
-
-
-    }
-
-}
-abstract class 무기{
-    abstract void 공격();
-}
-
-class 몽둥이 extends 무기 {
-    void 공격(){
-        System.out.println("몽둥이로 공격합니다.");
+        사람 a사람 = new 사람();
 
     }
 }
 
-class 칼 extends 무기{
-    void 공격(){
-        System.out.println("칼로 공격합니다.");
-    }
-}
+class 사람 {
+    private int 나이;
+    private String 이름;
+    private String 거주지;
 
-class 활 extends 무기{
-    void 공격(){
-        System.out.println("활로 공격합니다.");
+    public void 자기소개() {
+        System.out.println("안녕하세요, 제 이름은 " + 이름 + "이고, 나이는 " + 나이 + "살입니다.");
     }
-}
 
-class 도끼 extends 무기 {
-    void 공격(){
-        System.out.println("도끼로 공격합니다.");
+    // 단축키 alt + insert
+    // 게터/세터 자동 생성 가능
+    public int get나이() {
+        return 나이;
+    }
+
+    public void set나이(int 나이) {
+        this.나이 = 나이;
+    }
+
+    public String get이름() {
+        return 이름;
+    }
+
+    public void set이름(String 이름) {
+        this.이름 = 이름;
+    }
+
+    public String get거주지() {
+        return 거주지;
+    }
+
+    public void set거주지(String 거주지) {
+        this.거주지 = 거주지;
     }
 }
