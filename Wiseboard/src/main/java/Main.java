08af63f20943375dcbe2c1fb640bebaadd8cf304
RkdJIHYE <kangbs210210@gmail.com>
2026-01-30 09:30:36 +0900

    Fix:명언게시판 수정

--- Wiseboard/src/main/java/Main.java
+++ Wiseboard/src/main/java/Main.java
@@ -2,14 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        //객체 인스턴스 생성
 
-        WiseSaying wiseSaying = new WiseSaying();
         //스캐너를 이용하여, 키보드에 입력을 받는다.
         Scanner sc = new Scanner(System.in);
         //1단계
         System.out.println("==명언 탭==");
         int last_cnt = 0;
+        //객체 인스턴스 생성
+        WiseSaying[] wiseSayings = new WiseSaying[10];
+        int last_idx = 0;
+
 
         while (true){
             System.out.print("명령) ");
@@ -24,17 +26,24 @@ public class Main {
                 String content = sc.nextLine();
                 System.out.print("작가 : ");
                 String author = sc.nextLine();
+                WiseSaying wiseSaying = new WiseSaying();
 
                 wiseSaying.cnt = last_cnt;
                 wiseSaying.content = content;
                 wiseSaying.Author = author;
+
+                wiseSayings[last_idx]=wiseSaying;
+                last_idx++;
 
                 System.out.println(last_cnt+"번이 등록되었습니다.");
             }
             else if (cmd.equals("목록")){
                 System.out.println("번호 / 작가 / 명언");
                 System.out.println("----------------------");
-                System.out.println(wiseSaying.cnt+" / "+ wiseSaying.Author+" / "+wiseSaying.content);
+
+                for(int k=0;k<last_idx;k++){
+                   System.out.println(wiseSayings[k].cnt+" / "+wiseSayings[k].Author+" / "+wiseSayings[k].content);
+                }
             }
 
         }
