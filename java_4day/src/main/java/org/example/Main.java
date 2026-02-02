e350262ad5fdb0975c2dcb5cce25f85d2f8bff02
RkdJIHYE <kangbs210210@gmail.com>
2026-02-02 14:06:55 +0900

    Feat: Stream

--- java_4day/src/main/java/org/example/Main.java
+++ java_4day/src/main/java/org/example/Main.java
@@ -1,24 +1,62 @@
 package org.example;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
 
-public class Main{
+class Main {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+        List<Person> people = new ArrayList<>();
+        people.add(new Person(1, "Alice", 20, 'F'));
+        people.add(new Person(4, "Bob", 25, 'M'));
+        people.add(new Person(3, "David", 35, 'M'));
 
-        //명령형
-        for (int i =0;i< arr.length;i++){
-            System.out.println(arr[i]);
+
+        // 조건에 맞는 Person 객체가 없을 수도 있기 때문에 바로 Person 반환이 안된다. Optional을 통해 null처리 강제
+        Optional<Person> target = people.stream()
+                .filter(p -> p.getId() == 3)
+                .findFirst();
+
+        if(target.isPresent()) {
+            Person realTarget = target.get();
+            System.out.println(realTarget.getName());
         }
 
-        //선언형
-        Arrays.stream(arr)
-                .forEach(System.out::println);
+        Person target2 = people.stream()
+                .filter(p -> p.getId() == 3)
+                .findFirst()
+                .orElseThrow(); // 없으면 예외를 발생시킬 수도 있다.
 
-        //함수 ();-> 함수 실행
-        //함수 -> 함수 자체 
+    }
+}
+
+class Person {
+    private int id;
+    private String name;
+    private int age;
+    private char gender;
+
+    public Person(int id, String name, int age, char gender) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public char getGender() {
+        return gender;
     }
 }
