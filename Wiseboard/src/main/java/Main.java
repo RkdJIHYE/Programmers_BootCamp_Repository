51fd7812fe9eb3b56a98fa170de57c0c0e248042
RkdJIHYE <kangbs210210@gmail.com>
2026-02-03 10:15:08 +0900

    Feat:명언게시판 핵심로직 나누기

--- Wiseboard/src/main/java/Main.java
+++ Wiseboard/src/main/java/Main.java
@@ -1,52 +1,8 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args){
 
-        //스캐너를 이용하여, 키보드에 입력을 받는다.
-        Scanner sc = new Scanner(System.in);
-        //1단계
-        System.out.println("==명언 탭==");
-        int last_cnt = 0;
-        //객체 인스턴스 생성
-        WiseSaying[] wiseSayings = new WiseSaying[10];
-        int last_idx = 0;
-
-
-        while (true){
-            System.out.print("명령) ");
-            String cmd = sc.nextLine();
-            if (cmd.equals("종료")){
-                break;
-            }
-            //2단계 (등록)
-            if (cmd.equals("등록")) {
-                ++last_cnt;
-                System.out.print("명언 : ");
-                String content = sc.nextLine();
-                System.out.print("작가 : ");
-                String author = sc.nextLine();
-                WiseSaying wiseSaying = new WiseSaying();
-
-                wiseSaying.cnt = last_cnt;
-                wiseSaying.content = content;
-                wiseSaying.Author = author;
-
-                wiseSayings[last_idx]=wiseSaying;
-                last_idx++;
-
-                System.out.println(last_cnt+"번이 등록되었습니다.");
-            }
-            else if (cmd.equals("목록")){
-                System.out.println("번호 / 작가 / 명언");
-                System.out.println("----------------------");
-
-                for(int k=last_idx-1;k>=0;k--){
-                   System.out.println(wiseSayings[k].cnt+" / "+wiseSayings[k].Author+" / "+wiseSayings[k].content);
-                }
-            }
-
-        }
+        App app = new App();
+        app.run();
 
 
     }
