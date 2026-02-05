b9417db1ac04677d2b7198cb9f95cdb17c625135
RkdJIHYE <kangbs210210@gmail.com>
2026-02-05 15:34:52 +0900

    Feat:입력을 미리 받아두는법

---
+++ WiseBoardwithTDD/src/main/java/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner ("""
+                    등록
+                    과거에 집착하지 마라.
+                    작자미상
+                """);
+
+        String input = sc.nextLine();
+        String input2 = sc.nextLine();
+        String input3 = sc.nextLine();
+
+        System.out.println(input);
+        System.out.println(input2);
+        System.out.println(input3);
+
+    }
+}
