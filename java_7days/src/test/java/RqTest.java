2a29d31e141e611ab2de53838063a60006346755
RkdJIHYE <kangbs210210@gmail.com>
2026-02-05 14:13:43 +0900

    getParam -> green

--- java_7days/src/test/java/RqTest.java
+++ java_7days/src/test/java/RqTest.java
@@ -51,6 +51,14 @@ public class RqTest {
         assertThat(rst).isEqualTo("title");
     }
 
+    @Test
+    @DisplayName("value = 목록?keword=자바&keywordType=title일때, rg.getParam(\"keywordType\")->\"title\"")
+    void t5() {
+        Rq rq = new Rq("목록?keyword=자바&keywordType=title");
+        String rst = rq.getParam("keywordType");
+        assertThat(rst).isEqualTo("title");
+    }
+
 
 
 }
