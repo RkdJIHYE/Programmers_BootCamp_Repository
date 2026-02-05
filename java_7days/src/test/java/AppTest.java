e0d83ea2a5e22f14b2cdaf18d8d1d75ad34192ea
RkdJIHYE <kangbs210210@gmail.com>
2026-02-05 11:23:01 +0900

    Feat:TDD 연습

---
+++ java_7days/src/test/java/AppTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest {
+
+    @Test
+    @DisplayName("1+2=3")
+    void t1() {
+        App app = new App();
+        int rst = app.plus(1, 2);
+
+        assertThat(rst).isEqualTo(3);
+    }
+
+}
