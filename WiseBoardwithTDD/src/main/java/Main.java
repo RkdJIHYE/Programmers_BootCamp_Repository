8028f67576bcb3b34defd8f3460bd9f681a799c1
RkdJIHYE <kangbs210210@gmail.com>
2026-02-05 15:47:18 +0900

    Feat:출력을 모니터 대신 따로 배열에 저장 해놓은 후 -> 출력시켜보기

--- WiseBoardwithTDD/src/main/java/Main.java
+++ WiseBoardwithTDD/src/main/java/Main.java
@@ -1,8 +1,36 @@
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+       lab2();
+    }
+
+    public static void lab2(){
+        PrintStream Original_out = System.out;
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+        PrintStream printStream = new PrintStream(byteArrayOutputStream);
+
+
+        System.setOut(printStream);
+
+        System.out.println("hihi");
+        System.out.println("byebye");
+        System.out.println("ok");
+
+
+        System.setOut(Original_out);
+
+        System.out.println(byteArrayOutputStream);
+
+    }
+
+
+    public static void lab1(){
         Scanner sc = new Scanner ("""
                     등록
                     과거에 집착하지 마라.
