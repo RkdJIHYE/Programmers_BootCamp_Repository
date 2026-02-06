2a653899b6288dca2a29af98cb27499fa5dc50b5
RkdJIHYE <kangbs210210@gmail.com>
2026-02-06 11:32:06 +0900

    Feat:명언게시판 -> 1단계 TDD 구현

---
+++ WiseBoardwithTDD/src/main/java/App.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class App {
+
+    private Scanner scanner;
+    public App(Scanner scanner){
+        this.scanner=new Scanner (System.in);
+    }
+
+
+    public void run(){
+        System.out.println("== 명언 앱==");
+    }
+}
--- WiseBoardwithTDD/src/test/java/AppTest.java
+++ WiseBoardwithTDD/src/test/java/AppTest.java
@@ -1,6 +1,29 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Scanner;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class AppTest {
 
+    @Test
+    @DisplayName("== 명언 앱 == '명령'")
+    void t1(){
+        //input 으로 종료 넣어주고
+        Scanner sc = TestUtil.getScanner("종료");
 
+        ByteArrayOutputStream outputStream = TestUtil.setOutByteArray();
+        new App(sc).run();
+
+
+        String out = outputStream.toString();
+
+        assertThat(out).contains("== 명언 앱==");
+
+
+    }
 
 
 }
