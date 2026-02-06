30ecfb6114a3d180b8df0f4a55a2b76337575e75
RkdJIHYE <kangbs210210@gmail.com>
2026-02-06 12:41:15 +0900

    Feat:controller 추가

--- WiseBoardwithTDD/src/main/java/App.java
+++ WiseBoardwithTDD/src/main/java/App.java
@@ -1,8 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
 
     private Scanner sc;
+    List<WiseSaying> wiseSayings = new ArrayList<>();
+    int lst_id =0;
 
     public App(Scanner sc) {
         this.sc  = sc;
@@ -11,10 +15,14 @@ public class App {
     public void run() {
 
 
+
         System.out.println("== 명언 앱 ==");
         while(true) {
             System.out.println("명령) ");
             String cmd = sc.nextLine();
+
+
+
 
             switch (cmd) {
                 case "등록" -> {
@@ -22,8 +30,25 @@ public class App {
                     String saying = sc.nextLine();
                     System.out.print("작가 : ");
                     String author = sc.nextLine();
-                    System.out.println("1번 명언이 등록되었습니다.");
+                    lst_id++;
+                    WiseSaying wiseSaying = new WiseSaying(lst_id, saying, author);
+                    wiseSayings.add(wiseSaying);
+                    System.out.println("%d번 명언이 등록되었습니다.".formatted(lst_id));
                 }
+
+                case "목록"->{
+                    System.out.println("번호 / 작가 / 명언");
+                    System.out.println("----------------------");
+                    wiseSayings
+                            .reversed()
+                            .stream()
+                            .forEach(wiseSaying ->
+                                    System.out.printf("%d / %s / %s%n",
+                                    wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getSaying()));
+
+                }
+
+
                 case "종료" -> {
                     return;
                 }
