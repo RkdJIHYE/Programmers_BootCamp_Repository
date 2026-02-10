a530f3f616b5e91bd721ca0891ce6ee73b0a0f05
RkdJIHYE <kangbs210210@gmail.com>
2026-02-10 14:26:11 +0900

    Feat: 48강 green

--- WiseBoardwithTDD/src/main/java/com/back/standard/util/util/Util.java
+++ WiseBoardwithTDD/src/main/java/com/back/standard/util/util/Util.java
@@ -5,6 +5,7 @@ import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 
 public class Util {
     public static class file {
@@ -101,7 +102,19 @@ public class Util {
                 return false;
             }
         }
+        public static Stream<Path> walkRegularFiles(String dirPath, String fileNameRegex) {
+            try {
+                return Files.walk(Path.of(dirPath))
+                        .filter(Files::isRegularFile)
+                        .filter(path -> path.getFileName().toString().matches(fileNameRegex));
+            } catch (IOException e) {
+                return Stream.empty();
+            }
+        }
+
     }
+
+
 
     public static class json {
         public static String toString(Map<String, Object> map) {
