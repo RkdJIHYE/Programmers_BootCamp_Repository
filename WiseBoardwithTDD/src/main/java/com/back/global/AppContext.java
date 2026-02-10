e802826d7b5404b05c66c04a0ecda2e0f9ef3d48
RkdJIHYE <kangbs210210@gmail.com>
2026-02-10 10:45:17 +0900

    Feat:40강 red단계

--- WiseBoardwithTDD/src/main/java/com/back/global/AppContext.java
+++ WiseBoardwithTDD/src/main/java/com/back/global/AppContext.java
@@ -2,6 +2,7 @@ package com.back.global;
 
 import com.back.system.controller.SystemController;
 import com.back.wiseSaying.controller.WiseSayingController;
+import com.back.wiseSaying.repository.WiseSayingFileRepository;
 import com.back.wiseSaying.repository.WiseSayingRepository;
 import com.back.wiseSaying.service.WiseSayingService;
 
@@ -14,10 +15,12 @@ public class AppContext {
     public static WiseSayingController wiseSayingController;
     public static WiseSayingService wiseSayingService;
     public static WiseSayingRepository wiseSayingRepository;
-
+    public static WiseSayingFileRepository wiseSayingFileRepository;
     public static void init(Scanner _sc) {
         AppContext.sc = _sc;
+        //wiseSayginRepository -> 메모리버전
         AppContext.wiseSayingRepository = new WiseSayingRepository();
+        AppContext.wiseSayingFileRepository = new WiseSayingFileRepository();
         AppContext.wiseSayingService = new WiseSayingService();
         AppContext.wiseSayingController = new WiseSayingController(sc);
         AppContext.systemController = new SystemController();
