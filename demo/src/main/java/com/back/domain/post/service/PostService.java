c222fc18f53814a28d39e13a8b571c259d3f35e5
RkdJIHYE <kangbs210210@gmail.com>
2026-02-23 11:47:11 +0900

    Feat:service로 수정

---
+++ demo/src/main/java/com/back/domain/post/service/PostService.java
@@ -0,0 +1,34 @@
+package com.back.domain.post.service;
+
+import com.back.domain.post.entity.Post;
+import com.back.domain.post.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+    //작성하기
+    public Post write(String title, String content){
+        //객체로 생성
+        Post post = new Post(title,content);
+        return postRepository.save(post);
+    }
+
+    public Optional<Post> findById(int id){
+        return postRepository.findById(id);
+    }
+
+    public List<Post> findAll(){
+        return postRepository.findAll();
+    }
+
+    public long count(){
+        return postRepository.count();
+    }
+}
--- demo/src/main/java/com/back/global/BaseInitData.java
+++ demo/src/main/java/com/back/global/BaseInitData.java
@@ -1,26 +1,35 @@
 package com.back.global;
 
-import com.back.domain.wiseSaying.entity.Post;
-import com.back.domain.wiseSaying.repository.PostRepository;
+import com.back.domain.post.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BaseInitData {
+
+    @Autowired
+    private PostService postService;
+
     @Bean
-    ApplicationRunner initDataRunner(PostRepository postRepository) {
+    ApplicationRunner initDataRunner() {
         return args -> {
-
-            if (postRepository.count() > 0){
-                return;
-            }
-            System.out.println("초기 데이터를 로딩합니다.");
-
-            Post post1 = new Post("제목1","내용1");
-            postRepository.save(post1);
-
-            postRepository.findById(1);
+            work1();
+            work2();
         };
+    }
+
+    void work1() {
+        if(postService.count() > 0) {
+            return;
+        }
+        postService.write("제목1","내용1");
+        postService.write("제목2","내용2");
+    }
+
+    void work2() {
+        postService.findById(1);
+        // select * from post where id = 1;
     }
 }
