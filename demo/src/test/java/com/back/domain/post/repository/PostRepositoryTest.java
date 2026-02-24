6e4ef65fba930a86ba41b52dda95ebbb5ee36b1d
RkdJIHYE <kangbs210210@gmail.com>
2026-02-24 11:09:53 +0900

    Feat: 트랜잭션 and Rollback

--- demo/src/test/java/com/back/domain/post/repository/PostRepositoryTest.java
+++ demo/src/test/java/com/back/domain/post/repository/PostRepositoryTest.java
@@ -4,10 +4,14 @@ import com.back.domain.post.entity.Post;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
+@Transactional
+@Rollback
 public class PostRepositoryTest {
 
     @Autowired
@@ -20,5 +24,23 @@ public class PostRepositoryTest {
         assertThat(post.getId()).isEqualTo(2);
         assertThat(post.getContent()).isEqualTo("내용2");
         assertThat(post.getTitle()).isEqualTo("제목2");
+    }
+
+    @Test
+    void t2(){
+
+        Post post = new Post("제목3","내용3");
+        Post savedPost = postRepository.save(post);
+
+        assertThat(savedPost.getId()).isNotNull();
+        assertThat(savedPost.getContent()).isEqualTo("내용3");
+        assertThat(savedPost.getTitle()).isEqualTo("제목3");
+
+    }
+
+    @Test
+    void t3(){
+        long cnt = postRepository.count();
+        assertThat(cnt).isEqualTo(2);
     }
 }
