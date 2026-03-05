cb09b6f222e2a5843967c411e53270787f9b7d4c
RkdJIHYE <kangbs210210@gmail.com>
2026-03-05 11:12:45 +0900

    Feat:새로운 프로젝트 초기세팅

---
+++ Spring_new_proj/src/main/java/com/back/spring_new_proj/SpringNewProjApplication.java
@@ -0,0 +1,13 @@
+package com.back.spring_new_proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNewProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNewProjApplication.class, args);
+    }
+
+}
---
+++ Spring_new_proj/src/test/java/com/back/spring_new_proj/SpringNewProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.spring_new_proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNewProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
