82643a36e58784eb6fefee8df6380b49b379b461
RkdJIHYE <kangbs210210@gmail.com>
2026-03-06 11:32:05 +0900

    Feat:기존에 java에서 작성했던 html 을 따로 html파일로 뺐음

--- Spring_new_proj/src/main/java/com/back/domain/post/controller/PostController.java
+++ Spring_new_proj/src/main/java/com/back/domain/post/controller/PostController.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 import java.util.stream.Collectors;
 
@@ -22,28 +22,28 @@ public class PostController {
     private final PostService postService;
 
     @GetMapping("/posts/write-form")
-    @ResponseBody
-    public String writeForm() {
 
-        return getWriteForm("", "", "");
+    public String writeForm() {
+        return "write";
+
     }
 
     @AllArgsConstructor
     public static class WriteRequestForm {
-        @Size(min=2, max=10, message = "3-제목은 2자 이상 10자 이하로 입력해주세요.")
+        @Size(min = 2, max = 10, message = "3-제목은 2자 이상 10자 이하로 입력해주세요.")
         @NotBlank(message = "1-제목은 필수입니다.")
         private String title;
 
         @NotBlank(message = "2-내용은 필수입니다.")
-        @Size(min=2, max=100, message = "4-내용은 2자 이상 100자 이하로 입력해주세요.")
+        @Size(min = 2, max = 100, message = "4-내용은 2자 이상 100자 이하로 입력해주세요.")
         private String content;
     }
 
     @PostMapping("/posts/write")
-    @ResponseBody
+
     public String write(@Valid WriteRequestForm form, BindingResult bindingResult) {
 
-        if(bindingResult.hasErrors()) {
+        if (bindingResult.hasErrors()) {
 
             String errorMessages = bindingResult.getFieldErrors()
                     .stream()
@@ -55,35 +55,13 @@ public class PostController {
                     .sorted()
                     .collect(Collectors.joining("\n"));
 
-            return getWriteForm(errorMessages, form.title, form.content);
+            // 템플릿 응답
+            return "write";
         }
 
         Post post = postService.write(form.title, form.content);
 
+        // 템플릿 응답
         return "%d번 글이 작성되었습니다.".formatted(post.getId());
     }
-
-    private String getWriteForm(String errorMessage, String title, String content) {
-        return """
-                <ul style="color:red">%s</ul>
-                <form method="post" action="/posts/write">
-                  <input type="text" name="title" value="%s" autoFocus>
-                  <br>
-                  <textarea name="content">%s</textarea>
-                  <br>
-                  <input type="submit" value="작성">
-                </form>
-                
-                <script>
-                    const li = document.querySelector("ul li");
-                    const errorFieldName = li.dataset.errorField;
-                
-                    if(errorFieldName.length > 0) {
-                        const form = document.querySelector("form");
-                        form[errorFieldName].focus();
-                    }
-                </script>
-                """.formatted(errorMessage, title, content);
-    }
-
 }
