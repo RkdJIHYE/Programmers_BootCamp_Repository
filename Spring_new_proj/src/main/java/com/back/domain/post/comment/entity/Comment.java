46d0869162622bc9fe508dec47e983d48fe39ef5
RkdJIHYE <kangbs210210@gmail.com>
2026-03-10 11:16:16 +0900

    Feat:46강

--- Spring_new_proj/src/main/java/com/back/domain/post/comment/entity/Comment.java
+++ Spring_new_proj/src/main/java/com/back/domain/post/comment/entity/Comment.java
@@ -3,12 +3,10 @@ package com.back.domain.post.comment.entity;
 import com.back.domain.post.post.entity.Post;
 import com.back.global.entity.BaseEntity;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
 
 @Entity
 @AllArgsConstructor
@@ -17,7 +15,10 @@ import lombok.NoArgsConstructor;
 public class Comment extends BaseEntity {
     private String content;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     private Post post;
 
+    public void update(String content) {
+        this.content = content;
+    }
 }
