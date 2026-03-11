66e4d2a3fe2bf58a2b5948286349ddc72cf06811
RkdJIHYE <kangbs210210@gmail.com>
2026-03-11 12:26:05 +0900

    Feat:ApiPopstController

--- Spring-api/src/main/java/com/back/global/rsData/RsData.java
+++ Spring-api/src/main/java/com/back/global/rsData/RsData.java
@@ -1,11 +1,9 @@
 package com.back.global.rsData;
 
-import com.back.domain.post.comment.dto.CommentDto;
-
 public record RsData(
         String msg,
         String resultCode,
-        CommentDto data
+        Object data
 ) {
 
 }
