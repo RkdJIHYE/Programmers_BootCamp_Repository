6751b80556cebe7120e2c1bee9b48c5666a99570
RkdJIHYE <kangbs210210@gmail.com>
2026-03-12 12:31:42 +0900

    Feat:Test용 Controller 생성

---
+++ Spring-api/src/main/java/com/back/domain/post/SpringApiApplication.java
@@ -0,0 +1,17 @@
+package com.back.domain.post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class SpringApiApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringApiApplication.class, args);
+    }
+
+}
